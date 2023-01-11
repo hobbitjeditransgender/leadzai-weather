25bc5da7dde4347b872d941bfacd4a979c8e44ca
Bernardo Lima <bernardomendeslima99@gmail.com>
2023-01-11 23:57:27 +0000

    chore: start up project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
           target="_blank"
+          className="App-link"
           rel="noopener noreferrer"
+          href="https://www.leadzai.com/"
         >
-          Learn React
+          Weather App
         </a>
       </header>
     </div>
