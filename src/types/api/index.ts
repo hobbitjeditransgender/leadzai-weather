bcff1907f1e1c2a9408c205ed62dba59ecd04a2b
Bernardo Lima <bernardomendeslima99@gmail.com>
2023-01-15 15:58:02 +0000

    feat: type api

---
+++ src/types/api/index.ts
@@ -0,0 +1,103 @@
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Sys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
+
+interface Temp {
+  day: number;
+  min: number;
+  max: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
+
+interface FeelsLike {
+  day: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
+export interface Daily {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: Temp;
+  feels_like: FeelsLike;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: Weather[];
+  clouds: number;
+  pop: number;
+  uvi: number;
+  rain?: number;
+}
+
+export interface WeekForecast {
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  daily: Daily[];
+}
+
+export interface Coord {
+  lon: number;
+  lat: number;
+}
+export interface LocationWeather {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+  daily?: Daily[];
+}
+
+export interface LocationWeatherList {
+  [key: string]: LocationWeather;
+}
