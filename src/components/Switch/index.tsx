cca741adbd10bf8a737fcebfefb3a2c83b63c2dd
Bernardo Lima <bernardomendeslima99@gmail.com>
2023-01-15 16:32:33 +0000

    feat: switch

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import './index.scss';
+
+interface Props {
+  leftOption: string;
+  rightOption: string;
+  onChange: (isChecked: boolean) => void;
+}
 
 // switch between Fahrenheit and Celsius
-const Switch: React.FC = () => {
-  return <div></div>;
+const Switch: React.FC<Props> = ({ leftOption, rightOption, onChange }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.checked);
+
+  return (
+    <div className="switch-wrapper">
+      <span>{leftOption}</span>
+      <label className="switch">
+        <input type="checkbox" defaultValue={'false'} onChange={handleChange} />
+        <span className="slider round"></span>
+      </label>
+      <span>{rightOption}</span>
+    </div>
+  );
 };
 
 export default Switch;
