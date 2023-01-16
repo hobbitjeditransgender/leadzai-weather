b6fa081eeed6cb28963157413a1438b36ce86a6c
Bernardo Lima <bernardomendeslima99@gmail.com>
2023-01-16 01:26:23 +0000

    fix: type checkbox

--- src/components/UnitSwitch/unitSwitch.test.tsx
+++ src/components/UnitSwitch/unitSwitch.test.tsx
@@ -3,9 +3,11 @@ import Switch from '.';
 import '@testing-library/jest-dom';
 
 test('checks switch default value off', () => {
-  render(<Switch leftOption="C" rightOption="F" onChange={() => {}} />);
+  render(<Switch leftOption="C" rightOption="F" onChange={() => null} />);
 
-  expect(screen.getByRole('checkbox').checked).toBeFalsy();
+  const checkbox = screen.getByRole('checkbox') as HTMLInputElement;
+
+  expect(checkbox.checked).toBeFalsy();
 });
 
 test('calls onChange function', async () => {
