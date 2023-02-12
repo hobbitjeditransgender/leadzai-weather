f4d28f26f06946608dadd14ff346307fbb016437
Bernardo Lima <bernardomendeslima99@gmail.com>
2023-02-12 21:54:57 +0000

    fix: remove unused types

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -14,16 +14,6 @@ interface Main {
   humidity: number;
 }
 
-interface Wind {
-  speed: number;
-  deg: number;
-  gust: number;
-}
-
-interface Clouds {
-  all: number;
-}
-
 interface Sys {
   type: number;
   id: number;
@@ -32,70 +22,12 @@ interface Sys {
   sunset: number;
 }
 
-interface Temp {
-  day: number;
-  min: number;
-  max: number;
-  night: number;
-  eve: number;
-  morn: number;
-}
-
-interface FeelsLike {
-  day: number;
-  night: number;
-  eve: number;
-  morn: number;
-}
-export interface Daily {
-  dt: number;
-  sunrise: number;
-  sunset: number;
-  moonrise: number;
-  moonset: number;
-  moon_phase: number;
-  temp: Temp;
-  feels_like: FeelsLike;
-  pressure: number;
-  humidity: number;
-  dew_point: number;
-  wind_speed: number;
-  wind_deg: number;
-  wind_gust: number;
-  weather: Weather[];
-  clouds: number;
-  pop: number;
-  uvi: number;
-  rain?: number;
-}
-
-export interface WeekForecast {
-  lat: number;
-  lon: number;
-  timezone: string;
-  timezone_offset: number;
-  daily: Daily[];
-}
-
-export interface Coord {
-  lon: number;
-  lat: number;
-}
 export interface LocationWeather {
-  coord: Coord;
   weather: Weather[];
-  base: string;
   main: Main;
-  visibility: number;
-  wind: Wind;
-  clouds: Clouds;
-  dt: number;
   sys: Sys;
-  timezone: number;
-  id: number;
   name: string;
-  cod: number;
-  daily?: Daily[];
+  timezone: number;
 }
 
 export interface LocationWeatherList {
